412f13a74fd3e56182144131ca3d62dded033870
TruptiKikani <trupti.micrasol@gmail.com>
2022-04-04 12:01:44 +0530

    color changes

--- common_guide.js
+++ common_guide.js
@@ -1,48 +1,43 @@
-let a = document.getElementsByClassName("tagParent");
-
-// console.log(a);
+let a = document.querySelector(".tagParent");
 
 for (let i = 0; i < a.length; i++) {
-    a[i].addEventListener("mouseover", function (e) {
-        if (e.target.children.length > 0) {
-            e.target.children[0].style.opacity = "100"
-        }
-    })
+  a[i].addEventListener("mouseover", function (e) {
+    if (e.target.children.length > 0) {
+      e.target.children[0].style.opacity = "100";
+    }
+  });
 
-    a[i].addEventListener("mouseout", function (e) {
-        if (e.target.children.length > 0) {
-            e.target.children[0].style.opacity = "0"
-        }
-    })
-
+  a[i].addEventListener("mouseout", function (e) {
+    if (e.target.children.length > 0) {
+      e.target.children[0].style.opacity = "0";
+    }
+  });
 }
 
 let dropdown = document.getElementsByClassName("Parent");
 
 let i;
-// console.log(dropdown);
+
 for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function () {
-        // console.log(this);
-        let dropdownContent = this.nextElementSibling;
-        // console.log(dropdownContent);
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "block";
-        }
-    });
+  dropdown[i].addEventListener("click", function () {
+    let dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
 }
 
 let collectOpt = document.getElementsByClassName("SCROLL");
-// console.log(collectOpt);
-for (i = 0; i < collectOpt.length; i++){
-    collectOpt[i].addEventListener("click", function (e) {
-        const elemId = e.target.id
-        // let OptContent = document.getElementsByClassName("Docs");
-        // console.log(OptContent[0]);
-        window.scrollTo(0, document.getElementsByClassName(`${elemId}`)[0].offsetTop -
-            document.getElementsByClassName('Header')[0].offsetHeight);
-    })
+for (i = 0; i < collectOpt.length; i++) {
+  collectOpt[i].addEventListener("click", function (e) {
+    e.target.style.color = "#8b82f1";
+    const elemId = e.target.id;
+    window.scrollTo(
+      0,
+      document.getElementsByClassName(`${elemId}`)[0].offsetTop -
+        document.getElementsByClassName("Header")[0].offsetHeight
+    );
+  });
 }
-
